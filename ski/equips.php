21e3f722f6ad7cf9aa3e7ddc39161a3eec1fa6f3
oriolpont99 <72862026+oriolpont99@users.noreply.github.com>
2021-04-07 17:55:12 +0200

    v1.1
    Filtres per categoria i filtres per text afegits

--- ski/equips.php
+++ ski/equips.php
@@ -45,12 +45,20 @@
     <div class="content">
       <h1>EQUIPAMENT VARIAT</h1>
       <label>Filtra per material: </label><input id="myInput" type="text" placeholder="Esquis, casc, botes..">
-      <section id="myList" class="material">
+      <br />
+<div class="filterBtns">
+<button class="btn active" onclick="filterSelection('all')">Tots</button>
+<button class="btn" onclick="filterSelection('esquis')">Esquis</button>
+<button class="btn" onclick="filterSelection('roba')">Roba</button>
+<button class="btn" onclick="filterSelection('botes')">Botes</button>
+<button class="btn" onclick="filterSelection('cascs')">Cascs</button>
+</div>
+      <section id="myList" class="material results">
           <?php
             $servername = "localhost";
             $username = "root";
             $password = "";
-            $dbname = "skiworld";
+            $dbname = "skiworld2";
 
             // Create connection
             $conn = new mysqli($servername, $username, $password, $dbname);
@@ -59,13 +67,94 @@
                 die("Connection failed: " . $conn->connect_error);
             }
 
-            $sql = "SELECT * FROM material";
+            $sql = "SELECT * FROM roba";
             $result = $conn->query($sql);
 
             if ($result->num_rows > 0) {
                 // output data of each row
                 while($row = $result->fetch_assoc()) {
-                    echo "<div class='producte1'><br><img style='width: 72%;' src='".$row['imatge2']."' /> <br>". $row["material"]. " <br> <strong>Ref.:</strong> ". $row["id_material"]. " <br> <strong>". $row["nom"]. " </strong><br> <strong>Talla: </strong>" . $row["talla"] . " <br> <strong>Preu: </strong>" .$row["preu"] ."€ <br><br></div> ";
+                    echo "<div id='roba' class='producte1 roba filterElements'><br><img style='width: 72%;' src='".$row['imatge']."' /> <br>" . $row["material"] . "<br> <strong>". $row["marca"]. " </strong><br> <strong>Ref.:</strong>" . $row["id_roba"]. " <br>  <strong>Preu: </strong>" .$row["preu"] ."€ <br><br></div> ";
+                }
+            } else {
+                echo "0 results";
+            }
+
+            $conn->close();
+            ?>
+            <?php
+            $servername = "localhost";
+            $username = "root";
+            $password = "";
+            $dbname = "skiworld2";
+
+            // Create connection
+            $conn = new mysqli($servername, $username, $password, $dbname);
+            // Check connection
+            if ($conn->connect_error) {
+                die("Connection failed: " . $conn->connect_error);
+            }
+
+            $sql = "SELECT * FROM esquis";
+            $result = $conn->query($sql);
+
+            if ($result->num_rows > 0) {
+                // output data of each row
+                while($row = $result->fetch_assoc()) {
+                    echo "<div id='esquis' class='producte1 esquis filterElements'><br><img style='width: 72%;' src='".$row['imatge']."' /> <br>" . $row["material"] . "<br> <strong>". $row["marca"]. " </strong><br> <strong>Ref.:</strong>" . $row["id_esqui"]. " <br>  <strong>Preu: </strong>" .$row["preu"] ."€ <br><br></div> ";
+                }
+            } else {
+                echo "0 results";
+            }
+
+            $conn->close();
+            ?>
+                      <?php
+            $servername = "localhost";
+            $username = "root";
+            $password = "";
+            $dbname = "skiworld2";
+
+            // Create connection
+            $conn = new mysqli($servername, $username, $password, $dbname);
+            // Check connection
+            if ($conn->connect_error) {
+                die("Connection failed: " . $conn->connect_error);
+            }
+
+            $sql = "SELECT * FROM cascs";
+            $result = $conn->query($sql);
+
+            if ($result->num_rows > 0) {
+                // output data of each row
+                while($row = $result->fetch_assoc()) {
+                    echo "<div id='cascs' class='producte1 cascs filterElements'><br><img style='width: 72%;' src='".$row['imatge']."' /> <br>" . $row["material"] . "<br> <strong>". $row["marca"]. " </strong><br> <strong>Ref.:</strong>" . $row["id_casc"]. " <br>  <strong>Preu: </strong>" .$row["preu"] ."€ <br><br></div> ";
+                }
+            } else {
+                echo "0 results";
+            }
+
+            $conn->close();
+            ?>
+                      <?php
+            $servername = "localhost";
+            $username = "root";
+            $password = "";
+            $dbname = "skiworld2";
+
+            // Create connection
+            $conn = new mysqli($servername, $username, $password, $dbname);
+            // Check connection
+            if ($conn->connect_error) {
+                die("Connection failed: " . $conn->connect_error);
+            }
+
+            $sql = "SELECT * FROM botes";
+            $result = $conn->query($sql);
+
+            if ($result->num_rows > 0) {
+                // output data of each row
+                while($row = $result->fetch_assoc()) {
+                    echo "<div id='botes' class='producte1 botes filterElements'><br><img style='width: 72%;' src='".$row['imatge']."' /> <br>" . $row["material"] . "<br> <strong>". $row["marca"]. " </strong><br> <strong>Ref.:</strong>" . $row["id_bota"]. " <br>  <strong>Preu: </strong>" .$row["preu"] ."€ <br><br></div> ";
                 }
             } else {
                 echo "0 results";
@@ -74,6 +163,48 @@
             $conn->close();
             ?>
       </section>
+        <script>
+         filterSelection("all");
+   function filterSelection(c) {
+      var x, i;
+      x = document.querySelectorAll(".filterElements");
+      if (c == "all") c = "";
+      Array.from(x).forEach(item => {
+         removeActiveClass(item, "show");
+         if (item.className.indexOf(c) > -1) addActiveClass(item, "show");
+      });
+   }
+   function addActiveClass(ele, name) {
+      var i, arr1, arr2;
+      arr1 = ele.className.split(" ");
+      arr2 = name.split(" ");
+      for (i = 0; i < arr2.length; i++) {
+         if (arr1.indexOf(arr2[i]) == -1) {
+            ele.className += " " + arr2[i];
+         }
+      }
+   }
+   function removeActiveClass(ele, name) {
+      var i, arr1, arr2;
+      arr1 = ele.className.split(" ");
+      arr2 = name.split(" ");
+      for (i = 0; i < arr2.length; i++) {
+         while (arr1.indexOf(arr2[i]) > -1) {
+            arr1.splice(arr1.indexOf(arr2[i]), 1);
+         }
+      }
+      ele.className = arr1.join(" ");
+   }
+   var filterBtns = document.querySelector(".filterBtns");
+   var btns = filterBtns.getElementsByTagName("button");
+   for (var i = 0; i < btns.length; i++) {
+      btns[i].addEventListener("click", function() {
+         var current = document.getElementsByClassName("active");
+         current[0].className = current[0].className.replace(" active", "");
+         this.className += " active";
+      });
+   }
+        </script>  
     </div>
     <footer>
       <div class="row">
--- ski/javascript/javascript.js
+++ ski/javascript/javascript.js
@@ -1,7 +1,7 @@
 window.addEventListener("load", filtres, false);
 
 
-function filtres(){
+function filtres() {
 	var x = document.getElementById("mobil-menu");
     if (x.style.display === "none") {
       x.style.display = "block";
@@ -18,3 +18,44 @@ function amagar() {
     x.style.display = "none";
   }
 }
+
+   filterSelection("all");
+   function filterSelection(c) {
+      var x, i;
+      x = document.querySelectorAll(".filterElements");
+      if (c == "all") c = "";
+      Array.from(x).forEach(item => {
+         removeActiveClass(item, "show");
+         if (item.className.indexOf(c) > -1) addActiveClass(item, "show");
+      });
+   }
+   function addActiveClass(ele, name) {
+      var i, arr1, arr2;
+      arr1 = ele.className.split(" ");
+      arr2 = name.split(" ");
+      for (i = 0; i < arr2.length; i++) {
+         if (arr1.indexOf(arr2[i]) == -1) {
+            ele.className += " " + arr2[i];
+         }
+      }
+   }
+   function removeActiveClass(ele, name) {
+      var i, arr1, arr2;
+      arr1 = ele.className.split(" ");
+      arr2 = name.split(" ");
+      for (i = 0; i < arr2.length; i++) {
+         while (arr1.indexOf(arr2[i]) > -1) {
+            arr1.splice(arr1.indexOf(arr2[i]), 1);
+         }
+      }
+      ele.className = arr1.join(" ");
+   }
+   var filterBtns = document.querySelector(".filterBtns");
+   var btns = filterBtns.getElementsByTagName("button");
+   for (var i = 0; i < btns.length; i++) {
+      btns[i].addEventListener("click", function() {
+         var current = document.getElementsByClassName("active");
+         current[0].className = current[0].className.replace(" active", "");
+         this.className += " active";
+      });
+   }
